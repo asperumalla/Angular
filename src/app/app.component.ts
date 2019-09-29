b79b2ce58ac18cb54bca1045914321bef6d3e4e3
asperumalla <alphasovereign@gmail.com>
2019-09-29 14:31:38 -0400

    Property Binding from Parent to  Child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBasics';
+  parentProperty = 'Im from parent Component' ;
+
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  template: `Value from ts file : {{name}}`,
+  template: `Value from ts file : {{name}}<br>
+            This value from Parent : {{childInputProperty}}
+  `,
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
 
   name : string = "This value is being displayed from ts file child component";
+  @Input() childInputProperty ;
   constructor() { }
 
   ngOnInit() {
