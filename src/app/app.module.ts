ee6e0ada3af8ef27a95de9a85de234efbae87280
asperumalla <alphasovereign@gmail.com>
2019-09-30 11:32:09 -0400

    Add Dynamic Routing to User Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { Routes, RouterModule} from '@angular/router'
 
 const appRoutes : Routes = [
   { path : 'home', component : HomeComponent},
-  { path : 'users', component : UsersComponent},
+  { path : 'users/:id', component : UsersComponent},
   { path : 'servers', component : ServersComponent  }
 ];
 @NgModule({
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  userId : string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+     this.userId =  this.route.snapshot.params['id'];
   }
+
 
 }
